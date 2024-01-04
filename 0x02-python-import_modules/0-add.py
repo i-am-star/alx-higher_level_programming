ebaef38345be9289d078e08565070ab53ca62cfb
i-am-star <adannaya649@gmail.com>
2024-01-04 16:10:44 +0100

    adds two numbers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-result = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    import add_0
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))

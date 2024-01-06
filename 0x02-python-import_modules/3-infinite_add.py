7dada3012ef39c3cdf903d1deb58276fa46bf592
i-am-star <adannaya649@gmail.com>
2024-01-06 18:20:05 +0100

    .

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-num = len(sys.argv) - 1
-argg = sys.argv
-calc = 0
+    num = len(sys.argv) - 1
+    argg = sys.argv
+    calc = 0
 
-for i in range(num):
-    calc = calc + int(argg[i + 1])
-print("{}".format(calc))
+    for i in range(num):
+        calc = calc + int(argg[i + 1])
+    print("{:d}".format(calc))
